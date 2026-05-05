ea5ccd524502c97bddae854ada41dc4f64fd6cda
German Kröger <gkroger11@gmail.com>
2026-05-05 20:23:02 +0200

    Commit Inicial

---
+++ src/main/java/com/zaknein/TicTacToeAPI/TicTacToeApiApplication.java
@@ -0,0 +1,13 @@
+package com.zaknein.TicTacToeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeApiApplication.class, args);
+	}
+
+}
