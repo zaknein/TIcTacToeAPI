ec4d9a84a94de34a5c501e344647188f7344f3f0
German Kröger <gkroger11@gmail.com>
2026-05-08 10:01:50 +0200

    games conroller

---
+++ src/main/java/com/zaknein/TicTacToeAPI/controller/GamesController.java
@@ -0,0 +1,52 @@
+package com.zaknein.TicTacToeAPI.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@AllArgsConstructor
+@RequestMapping("/games/")
+@RestController
+public class GamesController {
+
+    @PostMapping()
+    public String challengePlayer(@RequestBody String oponent) {
+        //TODO: process POST request
+        
+        return gameService.challengePlayer();
+    }
+    
+    // @GetMapping()
+    // public String getMethodName() {
+
+    //     return gameService.getMyGames();
+    // }
+
+    // @GetMapping("/{id}")
+    // public String getMethodName(@RequestParam Long id) {
+
+
+    //     return gameService.getAllGames(Long id);
+    // }
+
+    // @DeleteMapping("/{id}")
+    // public void deleteGame(@RequestParam Long id){
+
+    //     gameService.deleteGame(id);
+    // }
+    
+    
+
+
+
+
+}
